89f59dcfbc2c9bb0f4c427c090faebc375240cd2
Aexer <302801376@student.rocmondriaan.nl>
2022-06-26 14:06:15 +0200

    weatherapi werkt

--- projects/weatherapp/js/main.js
+++ projects/weatherapp/js/main.js
@@ -11,14 +11,15 @@ const windForce = document.querySelector('.wind-force');
 const windDirection = document.querySelector('.wind-direction');
 const windHumidity = document.querySelector('.humidity');
 const table = document.querySelector('.table');
-console.log(inputBtn);
 
 document.addEventListener('DOMContentLoaded', getAPI());
-document.addEventListener('DOMContentLoaded', loadContent());
-inputBtn.addEventListener('click', () => {
-  getAPI();
-  loadContent();
+inputBtn.addEventListener('click', getAPI())
+input.addEventListener('keypress', (event) => {
+  if (event.key === "Enter") {
+    getAPI()
+  }
 })
+
 async function getAPI() {
   const options = {
     method: 'POST',
@@ -26,49 +27,36 @@ async function getAPI() {
       'Content-Type': 'application/json',
     }
   };
-
+  
   if (input.value == '') {
     input.value = 'Netherlands';
   }
-  console.log('yes');
   
   const API = fetch(`https://api.weatherapi.com/v1/current.json?key=2d3df043ce0b46cb8da112702222506&q=${input.value}`, options);
   const jsonAPI = await API.then(response => response.json())
-  console.log(jsonAPI);
-  loadContent(jsonAPI)
+  
+  loadContent();
+  function loadContent() {
+    time.textContent = jsonAPI.location.localtime;
+    // Create temperature icon
+    weatherIcon.firstChild.src = jsonAPI.current.condition.icon;
+    weatherIcon.firstChild.alt = jsonAPI.current.condition.text;
+    // Create celsius title
+    celsius.firstChild.innerHTML = `${jsonAPI.current.temp_c}<sup>o</sup>C`;
+    // Create country title
+    country.firstChild.textContent = jsonAPI.location.country;
+    // Create country title
+    region.firstChild.textContent = jsonAPI.location.region;
+    // Create wind-force
+    windForce.lastChild.textContent = `${jsonAPI.current.wind_kph}km p/h`;
+    // Create wind-direction
+    windDirection.lastChild.textContent = jsonAPI.current.wind_dir;
+    // Create wind-humidity
+    windHumidity.lastChild.textContent = `${jsonAPI.current.humidity}%`;
+    // Append everything
+  }
 }
 
-
-function loadContent(jsonAPI) {
-  time.textContent = jsonAPI.location.localtime;
-  // Create temperature icon
-  const weatherIconImg = document.createElement('img');
-  weatherIconImg.src = jsonAPI.current.condition.icon;
-  weatherIconImg.alt = jsonAPI.current.condition.text;
-  // Create celsius title
-  const celsiusH1 = document.createElement('h1');
-  celsiusH1.innerHTML = `${jsonAPI.current.temp_c}<sup>o</sup>C`;
-  // Create country title
-  const countryH3 = document.createElement('h3');
-  countryH3.textContent = jsonAPI.location.country;
-  // Create country title
-  const regionh4 = document.createElement('h5');
-  regionh4.textContent = jsonAPI.location.region;
-  // Create wind-force
-  const windForceSpan = document.createElement('span');
-  windForceSpan.textContent = `${jsonAPI.current.wind_kph}km p/h`;
-  // Create wind-direction
-  const windDirectionSpan = document.createElement('span');
-  windDirectionSpan.textContent = jsonAPI.current.wind_dir;
-  // Create wind-humidity
-  const windHumiditySpan = document.createElement('span');
-  windHumiditySpan.textContent = `${jsonAPI.current.humidity}%`;
-  // Append everything
-  weatherIcon.append(weatherIconImg);
-  celsius.append(celsiusH1);
-  country.append(countryH3);
-  region.append(regionh4);
-  windForce.append(windForceSpan);
-  windDirection.append(windDirectionSpan);
-  windHumidity.append(windHumiditySpan);
+function toggleTemp() {
+  
 }
